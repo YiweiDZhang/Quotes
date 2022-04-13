39e21bd2bdf90e98db73b209403a63014dee80d8
YiweiZhang-IP <yiwei.zhang@intelligentpathways.com.au>
2022-04-14 01:13:23 +1000

    Send email func

--- Quotes/ViewController/ViewController.swift
+++ Quotes/ViewController/ViewController.swift
@@ -5,9 +5,10 @@
 //  Created by Yiwei Zhang on 12/4/2022.
 //
 
+import MessageUI
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
     
     private let dataService = DataService()
 
@@ -17,12 +18,39 @@ class ViewController: UIViewController {
         self.loadData()
     }
     
-    func loadData() {
+    private func loadData() {
         self.dataService.getQuote(completion: { quote in
-            guard quote != nil else { return }
+            guard let quote = quote else { return }
             
-            
+            DispatchQueue.main.async {
+                self.sendEmail(quote: quote)
+            }
         })
+    }
+    
+    private func sendEmail(quote: Quotes) {
+        let recipients = ["thegein@gmail.com", "thegein@hotmail.com"]
+        
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients(recipients)
+            
+            if let subject = quote.title {
+                mail.setSubject(subject)
+            }
+            
+            let body = [quote.quote, quote.author]
+                .compactMap { $0 }
+                .joined(separator: "\n\n")
+            mail.setMessageBody(body, isHTML: false)
+            
+            present(mail, animated: true)
+        }
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true)
     }
 
 }
